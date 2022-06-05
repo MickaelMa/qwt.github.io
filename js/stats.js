90cdfc5477c07796acbda165a0f5463bcfc9ab92
Mickael <mickael.manteau@gmail.com>
2022-06-05 22:16:35 +0200

    ajout stats gen1

--- js/stats.js
+++ js/stats.js
@@ -1,3 +1,21 @@
+async function getNbMintGen1(gen1NFT, startTimestamp, endTimestamp) {
+    let result;
+
+    // preparation des variables
+    var urlAPI = "https://api.elrond.com/accounts/erd1qqqqqqqqqqqqqpgqjd0meg3aurn2utx5evcu87fvyfscs5cs83gqmeefmu/transactions/count?status=success&function=" + gen1NFT.mintFunction;
+    if (startTimestamp !== null) {
+        urlAPI += "&after="+ startTimestamp;
+    }
+    if (endTimestamp !== null) {
+        urlAPI += "&before="+ endTimestamp;
+    }
+
+    // sauvegarde du resultat 
+    gen1NFT.count = await getAPIResult(urlAPI);
+     
+}
+
+
 async function getNbMintGen0(gen0NFT, startTimestamp, endTimestamp) {
     let result;
 
@@ -15,6 +33,8 @@ async function getNbMintGen0(gen0NFT, startTimestamp, endTimestamp) {
      
 }
 
+
+
 // recuperation des NFT QWTCoin
 async function getNbQWTCoinMint(){
 
@@ -25,7 +45,8 @@ async function getNbQWTCoinMint(){
     var totalQWTCoin = data.reduce(function (sum, tax) {
         return parseInt(sum) + parseInt(tax.balance);
     }, 0);
-    var scQWTCoin = data.find(el => el.address === "erd1qqqqqqqqqqqqqpgq0jdfyzt2afk3dc35y9s8q0t0qk2tc56a83gq6ftr00").balance;
+
+    var scQWTCoin = data.find(el => el.address === "erd1qqqqqqqqqqqqqpgq4htcyjd0jnxumw0add2qy4xres2f6ukh83gqmzy3vv").balance;
     var qWTCoinMint = totalQWTCoin - scQWTCoin;
 
      $("#nbQWTCoin").append("QWT Coin NFT claimed : " + qWTCoinMint  + "/" + totalQWTCoin);
